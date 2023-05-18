a98dc1a3177c48bc0fc375df4ed1da0113527ced
avaki11 <azzavaki@gmail.com>
2023-05-18 12:35:32 +1000

    Added a new Crud practice in Go Lang

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies = []Movie{}
+
+func getMovie(response http.ResponseWriter, request *http.Request) {
+
+	response.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(response).Encode(movies)
+
+}
+
+func main() {
+	router := mux.NewRouter()
+	movies = append(movies, Movie{ID: "1", Isbn: "1234", Title: "Hello", Director: &Director{Firstname: "John", Lastname: "Pet"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "656", Title: "Commando", Director: &Director{Firstname: "Daniel", Lastname: "William"}})
+	movies = append(movies, Movie{ID: "3", Isbn: "1789", Title: "Rush Hour 1", Director: &Director{Firstname: "Jeff", Lastname: "Elder"}})
+	router.HandleFunc("/movies", createMovie).Methods("POST")
+	router.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+	router.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	router.HandleFunc("/movies", getAllMovies).Methods("GET")
+	router.HandleFunc("/movies/{id}", updateMovieByID).Methods("PUT")
+	fmt.Println("Listening on port 3000")
+	log.Fatal(http.ListenAndServe(":3000", router))
+
+}
