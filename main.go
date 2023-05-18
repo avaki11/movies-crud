05262258d9c88a0cf2d5138e24e2a9631f4c812a
avaki11 <azzavaki@gmail.com>
2023-05-19 07:22:16 +1000

    Add finished touches to REST api go

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -48,10 +50,36 @@ func deleteMovie(response http.ResponseWriter, request *http.Request) {
 	json.NewEncoder(response).Encode(movies)
 }
 
+func createMovie(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	json.NewDecoder(request.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000))
+	movies = append(movies, movie)
+	json.NewEncoder(response).Encode(movies)
+}
+
 func getAllMovies(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(response).Encode(movies)
 
+}
+
+func updateMovieByID(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(request)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(request.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(response).Encode(movies)
+			return
+		}
+	}
+	json.NewEncoder(response).Encode(movies)
 }
 
 func main() {
